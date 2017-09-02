106a03ba9e500c1a4733a7cae9a764dc8c2d2490
Jaroslove <jroslovemonro@gmail.com>
2017-09-02 22:52:50 +0300

    simple dynamic linq

---
+++ SortBuCol/DynamicOrder.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortBuCol
+{
+    static class DynamicOrder
+    {
+        public static IEnumerable<T> OrderBy<T>(this IEnumerable<T> source, string propertyName, string typeOfSort)
+        {
+            ParameterExpression expressino = Expression.Parameter(typeof(T), "people");
+            Expression propertyExpression = Expression.Property(expressino, propertyName);
+            var rezult = Expression.Lambda(propertyExpression, expressino);
+
+            var lambda = rezult.Compile();
+
+            Type enumType = typeof(Enumerable);
+            var methods = enumType.GetMethods(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static);
+            //var selmethod = methods.Where(m => m.Name == "OrderByDescending" && m.GetParameters().Count() == 2); ThenBy
+            var selmethod = methods.Where(m => m.Name == typeOfSort && m.GetParameters().Count() == 2);
+            var method = selmethod.First();
+            method = method.MakeGenericMethod(typeof(T), propertyExpression.Type);
+            var rez = (IEnumerable<T>)method.Invoke(null, new Object[] { source, lambda });
+            return rez.MyOrdering("LastName", "").MyOrdering("FirstName", "").MyOrdering("Patronymic", "");
+        }
+        public static IEnumerable<T> MyOrdering<T>(this IEnumerable<T> source, string propertyName, string typeOfSort)
+        {
+            ParameterExpression expressino = Expression.Parameter(typeof(T), "people");
+            Expression propertyExpression = Expression.Property(expressino, propertyName);
+            var rezult = Expression.Lambda(propertyExpression, expressino);
+
+            var lambda = rezult.Compile();
+
+            Type enumType = typeof(Enumerable);
+            var methods = enumType.GetMethods(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static);
+            //var selmethod = methods.Where(m => m.Name == "OrderByDescending" && m.GetParameters().Count() == 2); ThenBy
+            var selmethod = methods.Where(m => m.Name == "ThenBy" && m.GetParameters().Count() == 2);
+            var method = selmethod.First();
+            method = method.MakeGenericMethod(typeof(T), propertyExpression.Type);
+            var rez = (IEnumerable<T>)method.Invoke(null, new Object[] { source, lambda });
+            return rez;
+        }
+    }
+}
---
+++ SortBuCol/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SortBuCol
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        BindingSource source;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<Person> people = new List<Person>
+            {
+                new Person
+                {
+                    LastName = "One",
+                    FirstName = "Awo",
+                    Patronymic = "Sfsd",
+                    Birthday = new DateTime(1990, 2,3)
+                },
+                new Person
+                {
+                    LastName = "Ane",
+                    FirstName = "Bwo",
+                    Patronymic = "Efsd",
+                    Birthday = new DateTime(1993, 2,3)
+                },
+                new Person
+                {
+                    LastName = "Ane",
+                    FirstName = "Cwo",
+                    Patronymic = "Pfsd",
+                    Birthday = new DateTime(1990, 2,3)
+                },
+                new Person
+                {
+                    LastName = "Ane",
+                    FirstName = "Dwo",
+                    Patronymic = "Pfsd",
+                    Birthday = new DateTime(1990, 2,3)
+                },
+                new Person
+                {
+                    LastName = "Bne",
+                    FirstName = "Ewo",
+                    Patronymic = "Pfsd",
+                    Birthday = new DateTime(1993, 2,3)
+                }
+
+            };
+            source = new BindingSource();
+            source.DataSource = people;
+            dataGridView1.DataSource = source;
+        }
+        int i = 0;
+        private void dataGridView1_ColumnHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            string propertyName = dataGridView1.Columns[e.ColumnIndex].DataPropertyName;
+            List<Person> people = (List<Person>)source.DataSource;
+            //people = people.OrderBy(propertyName, "OrderByDescending").ToList();
+            if( i== 0)
+            {
+                people = people.OrderBy(propertyName, "OrderByDescending").ToList();
+                //people = people.MyOrdering(propertyName, "").ToList();
+            }
+            else
+            {
+                people = people.OrderBy(propertyName, "OrderBy").ToList();
+            }
+            source.DataSource = people;
+            i = 1;
+        }
+    }
+}
